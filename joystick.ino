26c1679420187d24ce92733903ffb5ba845a45a2
ESElectric <152034435+ESElectric@users.noreply.github.com>
2024-01-21 14:39:51 -0800

    Naming changes as well as y pos

--- joystick.ino
+++ joystick.ino
@@ -1,53 +1,52 @@
-# include <Servo.h>
-const int SW_pin = 2;
+#include <Servo.h>
+
+const int SW_pin = 2;//joy button
 const int X_pin = 0;
-const int y_pin = 1;
+const int Y_pin = 1;
 
-Servo bservo; //base servo 360
-Servo gservo; // grippy servo
-Servo servo1; // two servos
+Servo bservo; // Base servo 360
+Servo gservo; // Grippy servo
+Servo servo1; // Two servos
 Servo servo2;
-
 
 void setup() {
   pinMode(SW_pin, INPUT);
   digitalWrite(SW_pin, HIGH);
-  servo2.attach (11);
-  servo1.attach (12);
-  bservo.attach (13);
-  gservo.attach (14);
-  Serial.begin(9060);
+  servo2.attach(11);
+  servo1.attach(12);
+  bservo.attach(13);
+  gservo.attach(14);
+  Serial.begin(9600); 
 
 }
 
-
-
 void loop() {
-  int Xposition;
-  int Yposition; 
-  Xposition = analogRead(X_pin);
- 
-  int bservopos;
-  bservopos = map(Yposition, 0,1023, 0, 180);
+  int Xposition = analogRead(X_pin);
+  int Yposition = analogRead(Y_pin);
+
+  // Base servo movement
+  int bservopos = map(Yposition, 0, 1023, 0, 180);
   bservo.write(bservopos);
   delay(180);
-  // base servo movement
 
-  int  12pos;
-  12pos = map(Xposition,0,1023, 0, 180);
-  servo1.write(12pos);
-  servo2.write(12pos);
+  // Two servo movement
+  int servoPos = map(Xposition, 0, 1023, 0, 180);
+  servo1.write(servoPos);
+  servo2.write(servoPos);
   delay(180);
 
-  int gservo pos = 0;
-  button = digitalRead(SW_pin);
+  // Grippy servo movement
+  int gservoPos = 0;
+  int button = digitalRead(SW_pin);
 
-  if(button == HIGH){
-    if (gservo pos = 0){
+//claw
+  if (button == HIGH) {
+    if (gservoPos == 0) {
       gservo.write(180);
-    }
-    else{
+      gservoPos = 180; // Update the grippy servo position
+    } else {
       gservo.write(0);
+      gservoPos = 0; // Update the grippy servo position
     }
     delay(500);
   }
